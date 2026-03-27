6eb69f72eae4203e32b12f9b3b5188ed42283563
Daniel Müller <miladaniel370@gmail.com>
2026-03-27 19:02:56 -0300

    Fix: Reparando

--- src/main/java/com/weg/estrutura/pedagogica/Repository/ProdutoRepository.java
+++ src/main/java/com/weg/estrutura/pedagogica/Repository/ProdutoRepository.java
@@ -3,5 +3,16 @@ package com.weg.estrutura.pedagogica.Repository;
 import com.weg.estrutura.pedagogica.Model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    List<Produto> findByCategoriaId(Long categoriaId);
+
+    List<Produto> findByNomeContainingIgnoreCase(String nome);
+
+    Optional<Produto> findByIdAndNome(Long id, String nome);
+
+    List<Produto> findByCategoriaNomeContainingIgnoreCase(String nomeCategoria);
 }
