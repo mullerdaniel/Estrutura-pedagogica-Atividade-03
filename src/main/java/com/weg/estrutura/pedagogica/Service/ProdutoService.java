785e8bfbf712a635cf1e0c5dadbe5ed96efeba2f
Daniel Müller <miladaniel370@gmail.com>
2026-03-27 19:00:26 -0300

    Fix: Reparando

--- src/main/java/com/weg/estrutura/pedagogica/Service/ProdutoService.java
+++ src/main/java/com/weg/estrutura/pedagogica/Service/ProdutoService.java
@@ -3,14 +3,14 @@ package com.weg.estrutura.pedagogica.Service;
 
 import com.weg.estrutura.pedagogica.Model.Produto;
 
-public class ProdutoRespostaDto {
+public class ProdutoService {
 
     private Long id;
     private String nome;
     private Double preco;
     private String nomeCategoria;
 
-    public ProdutoRespostaDto(Produto produto) {
+    public ProdutoService(Produto produto) {
         this.id = produto.getId();
         this.nome = produto.getNome();
         this.preco = produto.getPreco();
